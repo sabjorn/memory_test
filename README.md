1c3095e2171e7c92893e18e0b758633ed611eb89
Steven A. Bjornson <info@sabjorn.net>
2016-03-13 11:58:16 -0700

    added python code and edited description in readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # memory_test
-the code for a sculpture comprised of a computer displaying the contents of it's memory to screen
+The code to run a laptop based sculpture.
+
+Python reads `/dev/mem` as a file, formats the data into an image frame, and then pushes to FFMEG using a named pipe. FFMEG then combines the frames into a video and streams to `localhost:1234`
+
+## Dependencies:
+* NumPy
+* PILLOW
+* FFMEG
+* VLC (or any video playback software capable of opening a network stream)
---
+++ mem.py
@@ -0,0 +1,35 @@
+#streams content of /dev/mem to VLC as RGB images.
+#NOTE: Needs to be run with sudo
+from PIL import Image
+import time
+from subprocess import Popen, PIPE #pipe data
+
+#16:9 aspect ratio
+x = 1600/16
+y = 900/16
+
+size = 4 * 1024 * 8 #8 pages of memory
+x = 2**8
+y = size/x
+#x = 2**8
+#	y = 2**7
+z = 4
+
+#fps = 2.66*4 #bpm!
+fps = 30.
+p = Popen(['ffmpeg', '-y', '-f', 'image2pipe', '-framerate', str(fps), \
+'-re', '-i', '-', '-tune', 'zerolatency','-b', '900k', '-c:v', 'libx264','-f', 'mpegts', '-r', str(fps), 'udp://localhost:1234'], stdin=PIPE) #stream output
+#p = Popen(['ffmpeg', '-y', '-f', 'image2pipe', '-framerate', '30', '-i', '-', '-pix_fmt', 'yuv420p', '-c:v', 'libx264', '-preset', 'slow', '-crf', '20', '-r', '30', 'memory.mov'], stdin=PIPE) #for saving to file
+
+# newfp = np.fromfile("/dev/mem", dtype='uint8', count=x*y*3, sep='')
+# im = np.reshape(np.copy(newfp),(x,y,3))
+	#for t in range(100):
+print('opening udp://localhost:1234')
+while (True):
+	#print (t)
+	newfp = np.fromfile("/dev/mem", dtype='uint8', count=x*y*z, sep='')
+	im = np.reshape(np.copy(newfp),(y,x,z))
+	im_out = Image.fromarray(np.uint8(im))
+	im_out.save(p.stdin,'PNG')
+	time.sleep(1/float(fps)) #pause to prevent rapid succession
+
