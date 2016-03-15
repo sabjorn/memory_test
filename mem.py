7b2043ab6b6475d9e42fc22b33adfe2318657a78
Steven A. Bjornson <info@sabjorn.net>
2016-03-14 21:04:24 -0700

    tried adding code from http://zulko.github.io/blog/2013/09/27/read-and-write-video-frames-in-python-using-ffmpeg/ to fix problem with ffmpeg on linux. It doesn't use image2pipe

--- mem.py
+++ mem.py
@@ -2,7 +2,8 @@
 #NOTE: Needs to be run with sudo
 from PIL import Image
 import time
-from subprocess import Popen, PIPE #pipe data
+#from subprocess import Popen, PIPE #pipe data
+import subprocess as sp
 
 #16:9 aspect ratio
 x = 1600/16
@@ -17,8 +18,24 @@ z = 4
 
 #fps = 2.66*4 #bpm!
 fps = 30.
-p = Popen(['ffmpeg', '-y', '-f', 'image2pipe', '-framerate', str(fps), \
-'-re', '-i', '-', '-tune', 'zerolatency','-b', '900k', '-c:v', 'libx264','-f', 'mpegts', '-r', str(fps), 'udp://localhost:1234'], stdin=PIPE) #stream output
+
+#code from http://zulko.github.io/blog/2013/09/27/read-and-write-video-frames-in-python-using-ffmpeg/
+#trying to get it to work.
+command = [ FFMPEG_BIN,
+        '-y', # (optional) overwrite output file if it exists
+        '-f', 'rawvideo',
+        '-vcodec','rawvideo',
+        '-s', '420x360', # size of one frame
+        '-pix_fmt', 'rgb24',
+        '-r', '24', # frames per second
+        '-i', '-', # The imput comes from a pipe
+        '-an', # Tells FFMPEG not to expect any audio
+        '-vcodec', 'mpeg',
+        'udp://localhost:1234' ]
+
+pipe = sp.Popen(command, stdin=sp.PIPE, stderr=sp.PIPE)
+#p = Popen(['ffmpeg', '-y', '-f', 'image2pipe', '-framerate', str(fps), \
+#'-re', '-i', '-', '-tune', 'zerolatency','-b', '900k', '-c:v', 'libx264','-f', 'mpegts', '-r', str(fps), 'udp://localhost:1234'], stdin=PIPE) #stream output
 #p = Popen(['ffmpeg', '-y', '-f', 'image2pipe', '-framerate', '30', '-i', '-', '-pix_fmt', 'yuv420p', '-c:v', 'libx264', '-preset', 'slow', '-crf', '20', '-r', '30', 'memory.mov'], stdin=PIPE) #for saving to file
 
 # newfp = np.fromfile("/dev/mem", dtype='uint8', count=x*y*3, sep='')
@@ -29,7 +46,7 @@ while (True):
 	#print (t)
 	newfp = np.fromfile("/dev/mem", dtype='uint8', count=x*y*z, sep='')
 	im = np.reshape(np.copy(newfp),(y,x,z))
-	im_out = Image.fromarray(np.uint8(im))
-	im_out.save(p.stdin,'PNG')
+    pipe.proc.stdin.write( image_array.tostring() )
+	#im_out = Image.fromarray(np.uint8(im))
+	#im_out.save(p.stdin,'PNG')
 	time.sleep(1/float(fps)) #pause to prevent rapid succession
-
