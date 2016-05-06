44994c3183000d42d3a6650873c1e7cdc3cff699
sabjorn <info@sabjorn.net>
2016-05-05 19:43:33 -0700

    code is working quite well. JPEG

--- README.md
+++ README.md
@@ -17,3 +17,6 @@ sudo nvram boot-args="kmem=1"
 ```
 
 And then reboot the system. This will likely have to be redone if NVRAM is ever reset or the system is updated.
+
+###Automatic Startup
+Two launchdd files were created, one to launch VLC in fullscreen and the other to run the python script. This will allow the 'sculpture' to launch when the computer starts. Information for setting up launchd can be found [here](http://launchd.info/). plist files are put in `/Library/LaunchDaemons` so they can be run as root (needed for the python application to read `/dev/mem`)
--- mem.py
+++ mem.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/ipython
 #streams content of /dev/mem to VLC as RGB images.
 #NOTE: Needs to be run with sudo
 from PIL import Image
@@ -12,14 +13,14 @@ z=4
 fps = 30.
 
 p = Popen(['ffmpeg', '-y', '-f', 'image2pipe', '-framerate', str(fps), \
-'-re', '-i', '-', '-tune', 'zerolatency','-b', '900k', '-c:v', 'libx264','-f', 'mpegts', '-r', str(fps), 'udp://localhost:1234'], stdin=PIPE, bufsize=size*4) #stream output
+'-re', '-i', '-', '-tune', 'zerolatency','-b', '900k', '-c:v', 'libx264', \
+'-f', 'mpegts', '-r', str(fps), 'udp://localhost:1234'], stdin=PIPE, bufsize=size*10) #stream output
+
 #p = Popen(['ffmpeg', '-y', '-f', 'image2pipe', '-framerate', '30', '-i', '-', '-pix_fmt', 'yuv420p', '-c:v', 'libx264', '-preset', 'slow', '-crf', '20', '-r', '30', 'memory.mov'], stdin=PIPE) #for saving to file
 
 print('opening udp://localhost:1234')
 while (True):
-	#print (t)
 	newfp = np.fromfile("/dev/mem", dtype='uint8', count=x*y*z, sep='')
 	im = np.reshape(np.copy(newfp),(y,x,z))
 	im_out = Image.fromarray(np.uint8(im))
-	im_out.save(p.stdin,'PNG')
-	time.sleep(1/float(fps)) #pause to prevent rapid succession
+	im_out.save(p.stdin, 'JPEG')
